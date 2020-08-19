0c5ebdfb90a5dc638e19f91af644cb0a5065185f
cnrstar <cnrstar@qq.com>
2020-08-19 13:39:20 +0800

    修复很多逻辑问题，现在不咋误报了

--- background.js
+++ background.js
@@ -3,35 +3,69 @@ const manifest = chrome.runtime.getManifest();
 const { version } = manifest;
 chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
-        const {url} = details;
-        const [protocal, u1] = url.split('://');
-        const [host] = u1.split('/');
-        const hostSplit = host.split('.');
 
-        const currentUrl = hostSplit[hostSplit.length - 2] + '.' + hostSplit[hostSplit.length - 1];
+        //url:当前的url；initiator：浏览器状态栏里的domain
+        let {url,initiator} = details;
+
+        //如果发起者为空，直接赋值url
+        if (typeof(initiator) == "undefined") {
+            initiator = url;
+        }
+        const protocal = url.split("://")[0];
+
+        //根据url返回域名和对应的path
+        function GetHostAndPath(url)
+        {
+          var arrUrl = url.split("//"); //  http://www.baidu.com/aaaa.php?aaa
+          var start = arrUrl[1].indexOf("/");
+          var host = arrUrl[1].substring(0,start);
+          var path = arrUrl[1].substring(start);//stop省略，截取从start开始到结尾的所有字符
+
+          var result =new Array(host,path);
+          return result
+        }
+        //获取主域名
+        function GetMainDomain(host)
+        {
+          var arrHost = host.split("."); //  a.b.c.baidu.com
+          var mainDomain = arrHost[arrHost.length - 2] + '.' + arrHost[arrHost.length - 1];
+          return mainDomain;
+        }
+        
+
+        const mainDomain = GetMainDomain(initiator);//浏览器状态栏的主域名
+        const targetHost = GetHostAndPath(url)[0]; //跨域或本域访问的目标主机
+        const targetPath = GetHostAndPath(url)[1];//跨域或本域访问的目标路径
+        const targetDomain = GetMainDomain(targetHost)  //目标主域名
 
         const WhiteList = ['baidu.com','qq.com','csdn.net','weibo.com','cnblogs.com']   //白名单
         for (const WhiteSite of WhiteList) {
-            if (currentUrl.indexOf(WhiteSite)>0){ 
+            if (mainDomain.includes(WhiteSite)){ 
                 return;
             }
         }
-        const path = u1.replace(currentUrl, '');
+
+
         let redirectUrl;
         let cancel;
-        console.log(path)
+        
+        if (mainDomain == targetDomain) //如果相等表示正常域内访问
         {
-            const data = JSON.parse(localStorage.data || '{}');
-            const blockQueryStringList = (data.blockQueryStringList || '').split(/\s+/g);
+            console.log(targetDomain);
+            return;
+        }
+        else                                    //如果不相等，可能是跨域访问，需要继续判断
+        {
+            const blockQueryStringList = ['callback','jsonp','javascript']
             for (const q of blockQueryStringList) {
                 if (protocal == 'http' || protocal == 'https') {
-                    if (q && path.includes(q)) {
+                    if (q && targetPath.includes(q)) {
+                        console.log(url);
                         redirectUrl = 'data:text/javascript;charset=UTF-8;base64,' + btoa(`;`);
                         new Notification('发现蜜罐！');
                     }
                 }
             }
-
         }
         if (cancel) return { cancel };
         else if (redirectUrl) return { redirectUrl }
