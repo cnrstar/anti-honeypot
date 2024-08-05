3df9af1d2e826f2228b3f8c9db3c58d5e3537253
cnrstar <cnrstar@qq.com>
2024-08-05 16:23:29 +0800

    优化代码逻辑，增加启动/暂停功能

--- background.js
+++ background.js
@@ -1,11 +1,61 @@
-'use strict'
-const manifest = chrome.runtime.getManifest();
-const {
-    version
-} = manifest;
-var count = 0
-chrome.webRequest.onBeforeRequest.addListener(
-    function (details) {
+'use strict';
+// 初始化插件状态和计数器
+let isEnabled = true;
+
+const currentVersion = chrome.runtime.getManifest().version;
+// 更新检查地址
+const updateUrl = 'http://chatcs.megavector.cn/antihonypot.txt';
+
+// 当插件安装或更新时，或者Chrome启动时，检查更新
+chrome.runtime.onInstalled.addListener(() => {
+    checkForUpdate();
+});
+
+chrome.runtime.onStartup.addListener(() => {
+    checkForUpdate();
+});
+
+// 检查更新的函数
+function checkForUpdate() {
+    fetch(updateUrl)
+        .then(response => response.text())
+        .then(text => {
+            // 假设txt文件内容是形如 "1.0.0" 的版本号字符串
+            const latestVersion = text.trim();
+            const v2 = parseInt(latestVersion.replace(/\./g, ''));
+            const v1 = parseInt(currentVersion.replace(/\./g, ''));
+            // 比较版本号，这里使用了简单的字符串比较，实际应用中可能需要更复杂的版本比较逻辑
+            if (v2 > v1) {
+                notifyUserToUpdate(latestVersion);
+            }
+        })
+        .catch(error => {
+            console.log('Error checking for update:', error);
+        });
+}
+
+// 通知用户更新的函数
+function notifyUserToUpdate(latestVersion) {
+    const options = {
+        type: 'basic',
+        iconUrl: 'img/logo.png', // 插件的图标
+        title: '插件更新可用',
+        message: `发现新版本 ${latestVersion}，请访问插件页面下载更新。`
+    };
+
+    chrome.notifications.create('updateNotification', options, () => {});
+}
+
+// 监听通知点击事件，当用户点击通知时，可以打开插件的页面
+chrome.notifications.onClicked.addListener(notificationId => {
+    if (notificationId === 'updateNotification') {
+        chrome.tabs.create({ url: 'chrome://extensions/' });
+    }
+});
+
+
+// 假设这是你用于处理网络请求的监听函数
+function handleWebRequest(details) {
         //url:当前的url；initiator：浏览器状态栏里的domain
         let {
             url,
@@ -17,23 +67,19 @@ chrome.webRequest.onBeforeRequest.addListener(
             initiator = url;
         }
         const protocal = url.split("://")[0];
-
+        
         //根据url返回域名和对应的path
         function GetHostAndPath(url) {
-            var arrUrl = url.split("//"); //  http://www.baidu.com/aaaa.php?aaa
-            var start = arrUrl[1].indexOf("/");
-            var host = arrUrl[1].substring(0, start);
-            var path = arrUrl[1].substring(start); //stop省略，截取从start开始到结尾的所有字符
-
-            var result = new Array(host, path);
-            return result
+            const urlObj = new URL(url);
+            return [urlObj.hostname, urlObj.pathname + urlObj.search];
         }
+        
         //根据url获取主域名
         function get_domain(url){
-            var domain;
-            var re_domain=/([a-zA-Z0-9-]+)(.com\b|.net\b|.edu\b|.miz\b|.biz\b|.cn\b|.cc\b|.org\b){1,}/g;
-            domain=url.match(re_domain);
-            return domain[0];
+            if (!url) return null;
+            var re_domain = /([a-zA-Z0-9-]+)(?:.com\b|.net\b|.edu\b|.miz\b|.biz\b|.cn\b|.cc\b|.org\b)/;
+            var domain = url.match(re_domain);
+            return domain ? domain[0] : null;
         }
         //判断host是否在黑名单内
         function inBlackList(host) {
@@ -41,10 +87,10 @@ chrome.webRequest.onBeforeRequest.addListener(
             const BlackList = ["account.itpub.net", "accounts.ctrip.com", "ajax.58pic.com", "api.csdn.net", "api.ip.sb", "api.passport.pptv.com", "bbs.zhibo8.cc", "bit.ly", "blog.csdn.net", "blog.itpub.net", "c.v.qq.com", "chinaunix.net", "cmstool.youku.com", "comment.api.163.com", "databack.dangdang.com", "dimg01.c-ctrip.com", "down2.uc.cn", "github.com", "hd.huya.com", "home.51cto.com", "home.ctfile.com", "home.zhibo8.cc", "hudong.vip.youku.com", "i.jrj.com.cn", "iask.sina.com.cn", "itunes.apple.com", "m.ctrip.com", "m.game.weibo.cn", "mapp.jrj.com.cn", "my.zol.com.cn","passport.ctrip.com", "passport.game.renren.com", "passport.iqiyi.com", "playbill.api.mgtv.com", "renren.com", "skylink.io", "u.faloo.com", "ucenter.51cto.com", "v.huya.com", "v2.sohu.com", "vote2.pptv.com", "wap.sogou.com", "webapi.ctfile.com", "weibo.com", "www.58pic.com", "www.iqiyi.com", "www.iteye.com", "www.zbj.com", "www.cndns.com", "mozilla.github.io", "www.sitestar.cn", "api.fastadmin.net", "m.site.baidu.com", "restapi.amap.com", "login.sina.com.cn", "now.qq.com", "message.dangdang.com", "musicapi.taihe.com", "api-live.iqiyi.com", "api.m.jd.com", "tie.163.com", "pcw-api.iqiyi.com", "so.v.ifeng.com", "passport.baidu.com", "wz.cnblogs.com", "passport.cnblogs.com", "hzs14.cnzz.com", "mths.be", "validity.thatscaptaintoyou.com", "stc.iqiyipic.com", "s14.cnzz.com", "sb.scorecardresearch.com", "js.cndns.com", "datax.baidu.com", "assets.growingio.com", "www.gnu.org", "wappassalltest.baidu.com", "baike.baidu.com", "ka.sina.com.cn", "p.qiao.baidu.com", "map.baidu.com", "www.dangdang.com", "g.alicdn.com", "s.faloo.com", "msg.qy.net", "morn.cndns.com", "i.qr.weibo.cn", "github.comgithub.com", "uis.i.sohu.com", "www.tianya.cn", "passport.mop.com", "commapi.dangdang.com", "comment.money.163.com", "chaxun.1616.net", "tieba.baidu.com", "remind.hupu.com", "service.bilibili.com", "node.video.qq.com", "api.weibo.com", "www.jiyoujia.com"];
             for (const BlackSite of BlackList) {
                 if (host == BlackSite) {
-                    return true
+                    return true;
                 }
             }
-            return false
+            return false;
         }
 
         var mainDomain = get_domain(initiator); //发起者的主域名
@@ -61,11 +107,9 @@ chrome.webRequest.onBeforeRequest.addListener(
         }
 
         let redirectUrl;
-        let cancel;
 
         if (mainDomain == targetDomain) { //如果相等表示正常域内访问
             console.log(targetDomain);
-            return;
         } else { //如果不相等，可能是跨域访问，需要继续判断
             const blockQueryStringList = ['callback', 'jsonp', 'javascript'];
             for (const q of blockQueryStringList) {
@@ -74,8 +118,7 @@ chrome.webRequest.onBeforeRequest.addListener(
                         redirectUrl = 'data:text/javascript;charset=UTF-8;base64,' + btoa(`;`);
                         if (inBlackList(targetHost)) {
                             // 黑名单拦截
-                            count += 1;
-                            new Notification('拦截黑名单' + count + '次：' + targetHost);
+                            new Notification('拦截黑名单' + targetHost);
                         } else {
                             // 普通拦截
                             new Notification('拦截可疑溯源请求：' + targetHost);
@@ -83,16 +126,43 @@ chrome.webRequest.onBeforeRequest.addListener(
                     }
                 }
             }
+
         }
-        if (cancel) return {
-            cancel
-        };
-        else if (redirectUrl) return {
+    if (redirectUrl) return {
             redirectUrl
         }
         else return {};
-    }, {
+}
+
+
+// 设置监听器
+chrome.webRequest.onBeforeRequest.addListener(
+    handleWebRequest,
+    {
         urls: ["<all_urls>"]
     },
     ["blocking"]
 );
+
+
+// 接收来自 popup.html 的消息
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.action === "toggleExtension") {
+        isEnabled = message.enabled;
+        if (isEnabled) {
+            // 如果插件之前被禁用，重新添加监听器
+            chrome.webRequest.onBeforeRequest.addListener(
+                handleWebRequest,
+                {
+                    urls: ["<all_urls>"]
+                },
+                ["blocking"]
+            );
+        } else {
+            // 如果插件被禁用，移除监听器
+            chrome.webRequest.onBeforeRequest.removeListener(handleWebRequest);
+        }
+    }
+});
+
+// 其他 background.js 代码逻辑...
--- popup.js
+++ popup.js
@@ -1,1 +1,38 @@
-//取消保存在session里
+// popup.js
+
+// 初始化插件状态为已启动
+let isEnabled = true;
+
+// 从 storage 中读取插件状态
+chrome.storage.sync.get('isEnabled', function(data) {
+    // 如果之前存储了状态，则使用该状态，否则默认为已启动
+    isEnabled = data.isEnabled !== undefined ? data.isEnabled : true;
+    updateButtonAndStatus();
+});
+
+// 更新按钮文本和状态显示
+function updateButtonAndStatus() {
+    const toggleButton = document.getElementById('toggleButton');
+    const statusElement = document.getElementById('status');
+    if (isEnabled) {
+        toggleButton.textContent = '暂停插件';
+        statusElement.textContent = '当前状态: 运行中';
+    } else {
+        toggleButton.textContent = '启动插件';
+        statusElement.textContent = '当前状态: 已暂停';
+    }
+}
+
+// 切换插件状态
+function toggleExtension() {
+    isEnabled = !isEnabled;
+    updateButtonAndStatus();
+    // 存储新的插件状态
+    chrome.storage.sync.set({isEnabled: isEnabled}, function() {
+        // 发送消息给 background.js 更新插件状态
+        chrome.runtime.sendMessage({action: "toggleExtension", enabled: isEnabled});
+    });
+}
+
+// 为按钮添加点击事件监听器
+document.getElementById('toggleButton').addEventListener('click', toggleExtension);
